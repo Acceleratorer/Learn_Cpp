f9dc905b9f93f0f55a4bfd7b037b7071aa58918f
Acceleratorer <noobtan77@gmail.com>
2025-09-16 14:24:50 +0700

    i add somethings i learn in this afternoon

---
+++ IT1_Year1/program2/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void tan()
+{
+    printf("Hello World\n");
+}
+
+void dumamay()
+{
+    printf("Chao Ni Ma\n");
+}
+int main()
+{   
+    int i;
+    for  ( i=0; i<5; i++)
+    {
+        dumamay();
+    }
+}
+
+
