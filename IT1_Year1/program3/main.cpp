57a214dcc5f39752b0659edd7a71c8a2d6d154ae
Acceleratorer <noobtan77@gmail.com>
2025-09-19 10:16:07 +0700

    add new program

--- IT1_Year1/program3/main.cpp
+++ IT1_Year1/program3/main.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 int main()
 {
-    int a=2;
-    int b=5;
-    int c=1;
-    int m = (a<b) ? ((a<c)? a:c) : ((b<c)? b:c);
-    cout<<m;
-    return 0;
+    int a;
+    cin>>a;
+    cout (a/100)%10<(a/10)%10? (a/10)%10:(a/100)%10;
+    return 0;    
+
 
    
 }
